20419a3450dce9ebc80cdc2c571e998dc64e610a
kanaksaini04 <kanaksaini07330@gmail.com>
2026-04-04 00:36:44 +0530

    Add files

---
+++ 2.Controlflow/Break.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var name = "GeeksforGeeks"
+    for (i in name){
+     print("$i")
+          if(i == 's') {
+            break
+          }
+    }
+}
---
+++ 2.Controlflow/continue.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    var num1 = 4
+    outer@ while (num1 > 0) {
+        num1--
+        var num2 = 4
+
+        inner@ while (num2 > 0) {
+            if (num1 <= 2)
+                continue@outer
+            println("num1 = $num1, num2 = $num2")
+            num2--
+        }
+    }
+}
