25ee6b730572c48cebe360af4482ff9444b84adb
kanaksaini04 <kanaksaini07330@gmail.com>
2026-04-04 00:38:27 +0530

    check

--- 2.Controlflow/continue.kt
+++ 2.Controlflow/continue.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>) {
 
         inner@ while (num2 > 0) {
             if (num1 <= 2)
-                continue@outer
+                continue@outer   // i will know
             println("num1 = $num1, num2 = $num2")
             num2--
         }
