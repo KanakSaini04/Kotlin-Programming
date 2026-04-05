b4d372ad855f5a73cc21b0b7860c03a71173b4ad
kanaksaini04 <kanaksaini07330@gmail.com>
2026-04-05 23:05:19 +0530

    Lambda Expression

---
+++ 4.Function/Lamda_Expression/Accessing_lambda.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val company = { println("GeeksforGeeks")}
+
+    // invoking functions:
+    
+    // Method1
+    company()  
+
+    // Method2
+    company.invoke() 
+}
