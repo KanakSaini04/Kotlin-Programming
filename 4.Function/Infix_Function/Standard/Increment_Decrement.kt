1911790f5d320304a9e4e7c4393307c4b37a245d
kanaksaini04 <kanaksaini07330@gmail.com>
2026-04-06 00:08:09 +0530

    Added infix function

---
+++ 4.Function/Infix_Function/Standard/Increment_Decrement.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var a = 8
+    var b = 4
+   
+    println(++a)      // call using infix notation
+    println(a.inc())  // call using dot and parenthesis
+    println(--b)      // call using infix notation
+    println(b.dec())  // call using dot and parenthesis
+}
