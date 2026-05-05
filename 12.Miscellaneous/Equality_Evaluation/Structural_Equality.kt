7708a9c76ffcdf31c5397cf962bbf576b8bc509e
kanaksaini04 <kanaksaini07330@gmail.com>
2026-05-05 23:45:01 +0530

    Update

--- 12.Miscellaneous/Equality_Evaluation/Structural_Equality.kt
+++ 12.Miscellaneous/Equality_Evaluation/Structural_Equality.kt
@@ -1,1 +1,8 @@
-//x?.equals(y) ?: (y === null)
+// x?.equals(y) ?: (y === null)
+// This states that,
+
+// If x is not null, it calls x.equals(y).
+// If x is null, it checks if y is also null using referential equality (===).
+// This means that == in Kotlin is null-safe and doesn't throw a NullPointerException.
+
+// Note: To use ==, the class should override the equals() method. In Kotlin, data classes automatically generate equals() (and hashCode()) based on their properties.
